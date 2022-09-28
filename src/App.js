e803760a576346cc1ddf8e9a802a040a28fcdd18
Ramzi <ramzibouzaiene00@gmail.com>
2022-09-28 22:40:21 +0100

    Add Skills section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
       <About/>
+      <Skills/>
     </div>
   );
 }
